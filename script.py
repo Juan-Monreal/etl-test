f9123580927898cc1f2251b5e74588fda282da3d
jmonrealr <jcmonrealr@gmail.com>
2022-08-04 10:23:20 -0500

    add: Utility functios to extract the data

--- script.py
+++ script.py
@@ -1,35 +1,79 @@
-import glob 
-import pandas as pd 
+import glob
+import pandas as pd
 from datetime import datetime
-
+import os
 
 tmpfile = "sifinETL_temp.tmp"
 logfle = "sifinETL_logfile.txt"
 targetFolder = "clean/"
+dataFolder = "data/"
+
+
+def createDir(path):
+    """
+    Function to create the required directories
+    If directory not exist
+    Using os library
+    """
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        else:
+            return True
+    except OSError:
+        print(f"ERROR: Creating directory with name {path}")
+
+
+def getFiles(extension, path):
+    """
+    Get all the files with the same extension
+    :param extension: extension file to be obtained
+    :param path: directory/path to check
+    :return: List with the path of files
+    """
+    return [os.path.join(path, file)
+            for file in os.listdir(path)
+            if file.endswith(extension)]
+
 
 def log(message):
+    """
+    Utility function to record the events occurring during the execution  of the script
+    It stores using timestamp + message
+    :param message:
+    """
     timestamp_format = '%H:%M:%S-%h-%d-%Y'
-    #Hour-Minute-Second-MonthName-Day-Year
-    now = datetime.now() # get current timestamp
+    # Hour-Minute-Second-MonthName-Day-Year
+    now = datetime.now()  # get current timestamp
     timestamp = now.strftime(timestamp_format)
-    with open("sifinETL_logfile.txt","a") as f: f.write(timestamp + ', ' + message + '\n')
+    with open("sifinETL_logfile.txt", "a") as f:
+        f.write(timestamp + ', ' + message + '\n')
+
 
 def extract_from_csv(file_path):
-	dataframe = pd.read_csv(file_path)
-	return dataframe
+    """
+    Utility function to extract the info inside a file in the format .csv
+    :param file_path: File containing the info to be extracted
+    :return: Pandas dataframe with the extracted data
+    """
+    dataframe = pd.read_csv(file_path)
+    return dataframe
 
 
 def extract(data):
+    """
+
+    :param data:
+    """
     pass
+
 
 def transform(data):
     pass
 
 
-
 def load(targetFile, data_to_load):
-	data_to_load.to_csv(targetFile)
-
+    data_to_load.to_csv(targetFile)
 
 
 log("ETL Job Started")
